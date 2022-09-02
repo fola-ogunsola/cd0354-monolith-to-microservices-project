59faeadf01296bb40bb8a0a2f4dd936c3d6fa51b
fola-ogunsola <ogunsolafolake1997@gmail.com>
2022-09-02 18:05:50 +0100

    s3 changes

--- udagram-api-feed/src/aws.ts
+++ udagram-api-feed/src/aws.ts
@@ -3,8 +3,12 @@ import {config} from './config/config';
 
 
 // Configure AWS
-const credentials = new AWS.SharedIniFileCredentials({profile: config.aws_profile});
-AWS.config.credentials = credentials;
+
+
+AWS.config.update({
+  accessKeyId: config.aws_secret_key,
+  secretAccessKey: config.aws_access_key
+});
 
 export const s3 = new AWS.S3({
   signatureVersion: 'v4',
